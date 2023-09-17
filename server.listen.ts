347d0bf971a50c5da19eef6eb5b60324420cc66c
Joe Hawkins <josephlhawkins2@gmail.com>
2023-09-17 22:06:48 +0100

    Change server log

--- server.listen.ts
+++ server.listen.ts
@@ -3,5 +3,5 @@ const app = require("./server");
 const { PORT = 8000 } = process.env;
 
 module.exports = app.listen(PORT, () => {
-  console.info(`Server Running on ${PORT}...`);
+  console.info(`[server] running on port ${PORT}...`);
 });
