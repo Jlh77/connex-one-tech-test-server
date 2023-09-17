09c8bdbe38b994d50d7e10926440b64de72753e0
Joe Hawkins <josephlhawkins2@gmail.com>
2023-09-17 22:08:08 +0100

    organise auth middleware

--- middlewares/auth.middleware.ts
+++ middlewares/auth.middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-export default (req: Request, res: Response, next: NextFunction) => {
+exports.authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   if (req.headers.authorization !== process.env.SECRET_TOKEN) {
     return res.status(403).send({ message: "Forbidden" });
   }
